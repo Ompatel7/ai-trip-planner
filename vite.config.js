3edaf15e132885a081757fbdc99534e778089e2a
Ompatel7 <ompatel2k3@gmail.com>
2025-01-06 11:31:33 +0530

    Update in viteConfig

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Moves React libraries into a separate chunk
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Increase the warning limit (optional)
+  },
   plugins: [react()],
   resolve: {
     alias: {
